02b0e8ff73473adde750207e4d7c5ef5bef9b57a
akshataBagoji <165755451+akshataBagoji@users.noreply.github.com>
2024-04-02 12:54:48 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ class test
 {
     public static void main(String [] args)
         {
-            System.out.println("hello gokak sencond time");
+            System.out.println("hello SEP ");
         }
 }
